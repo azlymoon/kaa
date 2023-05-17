a60bd46dbb0357d6871e61ed800dd7e5b1fa4943
m-fedosov <mifedosov@edu.hse.ru>
2023-05-17 21:56:33 +0300

    Не копировать в свою ветку (комментарии с лицензией symfony)
    Добавил тесты IpUtils для KPHP

---
+++ src/HttpFoundation/MyTests/testIpUtils.php
@@ -0,0 +1,22 @@
+<?php
+
+use Kaa\HttpFoundation\MyTests\IpUtilsTest;
+
+require __DIR__ . '/../../../vendor/autoload.php';
+
+$test = new IpUtilsTest();
+
+echo"\ntestIpV4\n";
+$test->testIpv4();
+
+echo"\ntestIpV6\n";
+$test->testIpv6();
+
+echo"\ntestInvalidIpAddressesDoNotMatch\n";
+$test->testInvalidIpAddressesDoNotMatch();
+
+echo"\ntestAnonymize\n";
+$test->testAnonymize();
+
+echo"\ntestIp4SubnetMaskZero\n";
+$test->testIp4SubnetMaskZero();
