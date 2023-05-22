45dbfac1b3a69d1df731a64db0ce67b6933c82e6
pvpender <ndmoiseev@edu.hse.ru>
2023-05-22 23:44:12 +0300

    cors

--- src/CorsBundle/CorsGenerator.php
+++ src/CorsBundle/CorsGenerator.php
@@ -4,15 +4,23 @@ namespace Kaa\CorsBundle;
 
 use Kaa\CodeGen\Attribute\PhpOnly;
 use Kaa\CodeGen\Contract\BootstrapProviderInterface;
+use Kaa\CodeGen\Contract\InstanceProviderInterface;
 use Kaa\CodeGen\Exception\CodeGenException;
 use Kaa\CodeGen\Exception\NoDependencyException;
 use Kaa\CodeGen\GeneratorInterface;
 use Kaa\CodeGen\ProvidedDependencies;
+use Kaa\DependencyInjection\Contract\InstanceProvider;
+use Kaa\DependencyInjection\DependencyInjectionGenerator;
 use Kaa\DependencyInjection\Exception\EventDispatcherLinkerException;
 
 #[PhpOnly]
 class CorsGenerator implements GeneratorInterface
 {
+    private const ADD_LISTENER_CODE = <<<'PHP'
+%dispatcher%->addListener('%eventName%', %callable%, %priority%);
+PHP;
+
+    private InstanceProvider $instanceProvider;
     private BootstrapProviderInterface $bootstrapProvider;
     public function __construct()
     {
@@ -26,6 +34,12 @@ class CorsGenerator implements GeneratorInterface
     public function generate(array $userConfig, ProvidedDependencies $providedDependencies): void
     {
         $this->bootstrapProvider = $this->getBootstrapProvider($providedDependencies);
+        $this->instanceProvider = $this->getInstanceProvider($providedDependencies);
+        $replacements = [
+            '%dispatcher%' => $this->instanceProvider->provideInstanceCode('kernel.dispatcher'),
+            '%eventName%' => 'http.kernel.find.action',
+            '%callable%' => "[, '']"
+        ];
         $this->bootstrapProvider->addCode("//var_dump(123);");
     }
 
@@ -44,5 +58,35 @@ class CorsGenerator implements GeneratorInterface
         }
 
         return $providedDependencies->get(BootstrapProviderInterface::class);
+    }
+
+    /**
+     * @throws NoDependencyException
+     * @throws CodeGenException
+     */
+    private function getInstanceProvider(ProvidedDependencies $providedDependencies): InstanceProvider
+    {
+        if (!$providedDependencies->has(InstanceProviderInterface::class)) {
+            EventDispatcherLinkerException::throw(
+                'You must execute %s before executing %s',
+                DependencyInjectionGenerator::class,
+                self::class
+            );
+        }
+
+        $instanceProvider = $providedDependencies->get(InstanceProviderInterface::class);
+        if (!$instanceProvider instanceof InstanceProvider) {
+            EventDispatcherLinkerException::throw(
+                'There is implementation %s of %s in provided dependencies, '
+                . 'but %s is compatible only with %s provided by %s',
+                $instanceProvider::class,
+                InstanceProviderInterface::class,
+                self::class,
+                InstanceProvider::class,
+                DependencyInjectionGenerator::class
+            );
+        }
+
+        return $instanceProvider;
     }
 }
--- src/CorsBundle/PvpenderCorsBundle.php
+++ src/CorsBundle/PvpenderCorsBundle.php
@@ -1,1 +1,27 @@
 <?php
+
+namespace Kaa\CorsBundle;
+
+use Kaa\HttpKernel\Request;
+use Kaa\HttpKernel\Response\Response;
+
+class PvpenderCorsBundle
+{
+    public static function checkOptions(\Kaa\EventDispatcher\EventInterface $event): void
+    {
+        $castedEvent = instance_cast($event, \Kaa\HttpKernel\Event\FindActionEvent::class);
+        if ($castedEvent === null) {
+            return;
+        }
+        $method = $castedEvent->getRequest()->method();
+        if ($method !== 'OPTIONS') {
+            return;
+        }
+        $castedEvent->setAction([PvpenderCorsBundle::class, 'returnEmptyResponse']);
+    }
+
+    public static function returnEmptyResponse(Request $request): Response
+    {
+        return new Response();
+    }
+}
