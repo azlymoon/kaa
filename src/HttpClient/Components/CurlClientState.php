bf54341ac3181b4477e7cd239587c8fcfd6908ac
Marat Omarov <marat@MacBook-Air-Marat-2.local>
2023-05-27 20:17:34 +0300

    Imported all files from CurlHttpClient KPHP library

---
+++ src/HttpClient/Components/CurlClientState.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Kaa\HttpClient\Components;
+
+class CurlClientState extends ClientState
+{
+    public ?CurlMultiHandle $handle;
+
+    /** @var PushedResponse[] */
+    public $pushedResponses = [];
+    /** @var DnsCache */
+    public $dnsCache;
+
+    public function __construct(int $maxHostConnections, int $maxPendingPushes)
+    {
+        $this->handle = new CurlMultiHandle();
+        $this->dnsCache = new DnsCache();
+        $this->reset();
+        if (\defined('CURLPIPE_MULTIPLEX')) {
+            $this->handle->curlSetOpt(\CURLMOPT_PIPELINING, \CURLPIPE_MULTIPLEX);
+        }
+        if (\defined('CURLMOPT_MAX_HOST_CONNECTIONS')) {
+            $maxHostConnections = $this->handle->curlSetOpt(\CURLMOPT_MAX_HOST_CONNECTIONS, 0 < $maxHostConnections ? $maxHostConnections : \PHP_INT_MAX) ? 0 : $maxHostConnections;
+        }
+        if (\defined('CURLMOPT_MAXCONNECTS') && 0 < $maxHostConnections) {
+            $this->handle->curlSetOpt(\CURLMOPT_MAXCONNECTS, $maxHostConnections);
+        }
+    }
+
+    public function reset(): void
+    {
+        foreach ($this->pushedResponses as $response) {
+            $this->handle->curlMultiRemoveHandle($response->handle->getHandle());
+            $response->handle->curlClose();
+        }
+
+        $this->pushedResponses = [];
+        $this->dnsCache->evictions = $this->dnsCache->evictions ?: $this->dnsCache->removals;
+        $this->dnsCache->removals = $this->dnsCache->hostnames = [];
+
+        // We need to add this functionality to runtime of KPHP.
+        //$this->handle->curlSetOpt(CURLMOPT_PUSHFUNCTION, null);
+        $active = 0;
+        while (CURLM_CALL_MULTI_PERFORM === $this->handle->curlMultiExec($active));
+    }
+
+    /**
+     * @param CurlHandle $parent
+     * @param $pushed
+     * @param array $requestHeaders
+     * @param int $maxPendingPushes
+     * @return int
+     */
+    public static function handlePush($parent, $pushed, array $requestHeaders, int $maxPendingPushes): int
+    {
+        $headers = [];
+        $origin = $parent->getInfo(\CURLINFO_EFFECTIVE_URL);
+
+        foreach ($requestHeaders as $h) {
+            if (false !== $i = strpos($h, ':', 1)) {
+                $headers[substr($h, 0, $i)][] = substr($h, 1 + $i);
+            }
+        }
+
+        if (!isset($headers[':method']) || !isset($headers[':scheme']) || !isset($headers[':authority']) || !isset($headers[':path'])) {
+            // опущена работа с логированием через поле $this->logger
+            //$this->logger?->debug(sprintf('Rejecting pushed response from "%s": pushed headers are invalid', $origin));
+            return \CURL_PUSH_DENY;
+        }
+
+        $url = $headers[':scheme'][0].'://'.$headers[':authority'][0];
+
+        if (!str_starts_with($origin, $url.'/')) {
+            //$this->logger?->debug(sprintf('Rejecting pushed response from "%s": server is not authoritative for "%s"', $origin, $url));
+            return \CURL_PUSH_DENY;
+        }
+
+        // Опущена работа с массивом $this->pushedResponses ввиду отсутствия функционала получения индекса через key().
+        // Нужно проработать передачу текущего элемента массива в функцию.
+
+//        if ($maxPendingPushes <= \count($this->pushedResponses)) {
+//            $fifoUrl = key($this->pushedResponses);
+//            unset($this->pushedResponses[$fifoUrl]);
+//            $this->logger?->debug(sprintf('Evicting oldest pushed response: "%s"', $fifoUrl));
+//        }
+
+        $url .= $headers[':path'][0];
+        //$this->logger?->debug(sprintf('Queueing pushed response: "%s"', $url));
+
+        // Опущено использование $this->openHandles из родительского класса.
+        // Нужно разобраться с данным полем, указать тип и предназначение.
+        //$this->pushedResponses[$url] = new PushedResponse(new CurlResponse($this, $pushed), $headers, $this->openHandles[(int) $parent][1] ?? [], $pushed);
+
+        return \CURL_PUSH_OK;
+    }
+}
---
+++ src/HttpClient/Components/CurlHandle.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Kaa\HttpClient\Components;
+
+class CurlHandle
+{
+    /** @var int $handle */
+    protected $handle;
+    public function __construct(?string $url = null, ?int $handle = null)
+    {
+        $handle ? $this->handle = $handle : $this->handle = curl_init($url);
+    }
+
+    /**
+     * @return int
+     */
+    public function getHandle()
+    {
+        return $this->handle;
+    }
+
+    /**
+     * @param int $option
+     * @param mixed $value
+     * @return bool
+     */
+    public function curlSetOpt(int $option, $value): bool
+    {
+        return curl_setopt($this->handle, $option, $value);
+    }
+
+//    public function curlPause(int $flag): int
+//    {
+//        return curl_pause($this->handle, $flag);
+//    }
+
+    public function curlSetOptArray(array $optins): bool
+    {
+        return curl_setopt_array($this->handle, $optins);
+    }
+
+    public function curlClose(): void
+    {
+        curl_close($this->handle);
+    }
+
+    public function getInfo(?int $option = null)
+    {
+        return curl_getinfo($this->handle, $option);
+    }
+}
